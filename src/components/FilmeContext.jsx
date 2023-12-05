c78fffd21528f4940a336b4d0523688cfa6a1678
danielcbarata <dcbarata@unifei.edu.br>
2023-12-05 14:41:41 -0300

    Adicionando busca dos filmes no bd

--- src/components/FilmeContext.jsx
+++ src/components/FilmeContext.jsx
@@ -8,8 +8,19 @@ export default function FilmeProvider({ children }) {
   const [faixa_etaria, setFaixa_Etaria] = useState('');
   const [duracao, setDuracao] = useState('');
   const [url_imagem, setUrl_Imagem] = useState('');
+  const [filmes, setFilmes] = useState([]);
+  const url = "https://banco-cine-verse.vercel.app/Filmes";
+
+  function buscarFilmes() {
+    fetch(url)
+      .then((respFetch) => respFetch.json())
+      .then((respJson) => setFilmes(respJson))
+      .catch((erro) => console.warn(erro))
+  }
 
   return (
-    
+    <FilmeContext.Provider value={{ titulo, sinopse, faixa_etaria, duracao, url_imagem, setTitulo, setSinopse, setFaixa_Etaria, setDuracao, setUrl_Imagem, buscarFilmes, filmes }} >
+      {children}
+    </FilmeContext.Provider>
   );
 }
--- src/screens/Escolha.jsx
+++ src/screens/Escolha.jsx
@@ -1,10 +1,54 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { StyleSheet, FlatList, View } from 'react-native'
+import React, { useContext, useEffect, useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { ListItem } from '@rneui/themed';
+import { Icon } from '@rneui/base';
+import { FilmeContext } from './FilmeContext';
 
-export default function Escolha() {
-  return (
-    <View>
-      <Text>Escolha</Text>
-    </View>
-  )
+export default function Escolha(props) {
+    
+    const {buscarFilmes, filmes} = useContext(FilmeContext);
+
+    useEffect(() => {
+        buscarFilmes();
+    }, []);
+
+    return (
+        <SafeAreaView>
+            <FlatList
+                data={filmes}
+                keyExtractor={item => item.id}
+                renderItem={({ item }) => (
+                    <ListItem bottomDivider>
+                        <ListItem.Content style={styles.item}>
+                            <View>
+                                <ListItem.Title>{item.titulo}</ListItem.Title>
+                                <ListItem.Subtitle>{item.sinopse}</ListItem.Subtitle>
+                            </View>
+                            <View style={{ width: 100 }}>
+                                <ListItem.ButtonGroup
+                                    buttons={[
+                                        <Icon name='edit' size={20} color={'blue'} onPress={() =>
+                                            props.navigation.navigate("Detalhe", { item })}/>,
+                                        <Icon name='trash-can' size={20} color={'red'} onPress={() =>
+                                            Alert.alert("teste")} />
+                                    ]}
+                                />
+                            </View>
+                        </ListItem.Content>
+                    </ListItem>
+                )}
+            >
+            </FlatList>
+        </SafeAreaView>
+    )
 }
+
+
+const styles = StyleSheet.create({
+    item: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+})
